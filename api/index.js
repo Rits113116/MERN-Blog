44cf57f0326e1d27d908fda8d83055554bfe567c
Rits113116 <rr4618634@gmail.com>
2024-07-07 11:24:44 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connected');
 }).catch((err)=>{
     console.log(err);
 })
+
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running at port 3000!');
 })
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
